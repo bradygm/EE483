2fbd177af6598e2004107f4bba1285b8cc6462ba
Moon <bradygmoon@gmail.com>
2018-10-30 20:11:15 -0600

    PID control and tuning for ball and beam

---
+++ ballOnBeam/hw10/beamController.m
@@ -0,0 +1,50 @@
+classdef beamController
+    % 
+    %    This class inherits other controllers in order to organize multiple controllers.
+    %
+    %----------------------------
+    properties
+        zCtrl
+        thetaCtrl
+        m2
+        g
+        ell
+        m1    
+        limit
+    end
+    %----------------------------
+    methods
+        %----------------------------
+        function self = beamController(P)
+            % Instantiates the SS_ctrl object
+            self.zCtrl = beamPIDControl(P.z0, P.kp_z, P.ki_z, P.kd_z, P.theta_max, P.beta, P.Ts);
+            self.thetaCtrl = beamPIDControl(P.theta0,P.kp_th, 0.0, P.kd_th, P.F_max, P.beta, P.Ts);
+            self.m2 = P.m2;
+            self.g = P.g;
+            self.ell = P.ell;
+            self.m1 = P.m1;
+            self.limit = P.F_max;
+        end
+        %----------------------------
+        function F = u(self, y_r, y)
+            % y_r is the referenced input
+            % y is the current state
+            z_r = y_r;
+            z = y(1);
+            theta = y(2);
+            % the reference angle for theta comes from the outer loop PD control
+            theta_r = self.zCtrl.PID(z_r, z, false);
+            % the force applied to the cart comes from the inner loop PD control
+            F_tilda = self.thetaCtrl.PID(theta_r, theta, false);
+            Ftemp = F_tilda + (self.m2*self.g*self.ell+2*self.m1*self.g*z)/(2*self.ell);
+            F = self.saturate(Ftemp);
+        end
+        %----------------------------
+        function out = saturate(self,u)
+            if abs(u) > self.limit
+                u = self.limit*sign(u);
+            end
+            out = u;
+        end
+    end
+end
--- ballOnBeam/hw10/beamPIDControl.m
+++ ballOnBeam/hw10/beamPIDControl.m
@@ -1,4 +1,4 @@
-classdef springPIDControl < handle
+classdef beamPIDControl < handle
     %----------------------------
     properties
         kp
@@ -12,34 +12,37 @@ classdef springPIDControl < handle
         y_d1
         error_dot
         error_d1
+        integrator
     end
     %----------------------------
     methods
         %----------------------------
-        function self = springPIDControl(kp, kd, beta, Ts)
+        function self = beamPIDControl(initialy, kp, ki, kd, limit, beta, Ts)
             self.kp = kp;                 % Proportional control gain
             self.ki = ki;
             self.kd = kd;                 % Derivative control gain
-%             self.limit = limit;           % The output will saturate at this limit
+            self.limit = limit;           % The output will saturate at this limit
             self.beta = beta;
             self.Ts = Ts;
 
             self.y_dot = 0.0;              % estimated derivative of y
-            self.y_d1 = 0.0;              % Signal y delayed by one sample
+            self.y_d1 = initialy;              % Signal y delayed by one sample
             self.error_dot = 0.0;          % estimated derivative of error
             self.error_d1 = 0.0;          % Error delayed by one sample
+            self.integrator = 0.0;
         end
-        %----------------------------
+         %----------------------------
         function u = PID(self, y_r, y, flag)
             %
-            %    PD control,
+            %    PID control,
             %    
             %    if flag==True, then returns
-            %        u = kp*error + kd*error_dot.
+            %        u = kp*error + ki*integral(error) + kd*error_dot.
             %    else returns 
-            %        u = kp*error - kd*y_dot.
+            %        u = kp*error + ki*integral(error) - kd*y_dot.
             %    
             %    error_dot and y_dot are computed numerically using a dirty derivative
+            %    the integral is computed numerically
             %
 
             % Compute the current error
@@ -49,18 +52,20 @@ classdef springPIDControl < handle
             % differentiate error and y
             self.differentiateError(error);
             self.differentiateY(y);
-
+%             self.integratorAntiWindup(error);
             % PD Control
             if flag==true
-%                 u_unsat = self.kp*error + self.kd*self.error_dot;
+                u_unsat = self.kp*error...
+                        + self.ki*self.integrator...
+                        + self.kd*self.error_dot;
             else
                 u_unsat = self.kp*error...
                         + self.ki*self.integrator...
                         - self.kd*self.y_dot;
             end
             % return saturated control signal
-            u = self.saturate(u_unsat);
-            self.integratorAntiWindup(u, u_unsat);
+            u = u_unsat;
+            
         end
         %----------------------------
         function u = PD(self, y_r, y, flag)
@@ -83,16 +88,15 @@ classdef springPIDControl < handle
 
             % PD Control
             if flag==true
-%                 u_unsat = self.kp*error + self.kd*self.error_dot;
+                u_unsat = self.kp*error + self.kd*self.error_dot;
             else
-                u_unsat = self.kp*error...
-                        - self.kd*self.y_dot;
+                u_unsat = self.kp*error - self.kd*self.y_dot;
             end
             % return saturated control signal
             u = u_unsat;
         end
         %----------------------------
-        function self = differentiateError(self, error)
+        function self = differentiateError(self, error)     
             self.error_dot = self.beta*self.error_dot + (1-self.beta)*((error - self.error_d1) / self.Ts);
             self.error_d1 = error;            
         end
@@ -103,23 +107,10 @@ classdef springPIDControl < handle
         end
         %----------------------------
         function self = integrateError(self, error)
-            self.integrator = self.integrator + (self.Ts/2)*(error+self.error_d1);
-        end
-        %----------------------------
-        function self = integratorAntiWindup(self, u_sat, u_unsat)
-            % integrator anti-windup
-            if self.ki~=0
-                self.integrator = self.integrator + self.Ts/self.ki*(u_sat-u_unsat);
+            if abs(self.y_dot < .01)
+                self.integrator = self.integrator + (self.Ts/2)*(error+self.error_d1);
             end
         end
-        %----------------------------
-        function out = saturate(self,u)
-            if abs(u) > self.limit
-                u = self.limit*sign(u);
-            end
-            out = u;
-        end
-
     end
 end
 
